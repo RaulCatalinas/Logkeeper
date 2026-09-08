d1dd7a6aff71875b17cc73d27f3b5f9fb5004aef
Raul Catalinas <raulcatalinas@gmail.com>
2026-09-09 00:07:22 +0200

    refactor: replace "final" keyword with "const"

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,10 +1,10 @@
 import 'log_level.dart' show LogLevel;
 
-final Map<LogLevel, String> colorMap = Map.from({
+const Map<LogLevel, String> colorMap = {
   LogLevel.info: '\x1B[36m',
   LogLevel.warning: '\x1B[33m',
   LogLevel.error: '\x1B[31m',
   LogLevel.critical: '\x1B[95m'
-});
+};
 
-final resetColor = '\x1B[0m';
+const String resetColor = '\x1B[0m';
